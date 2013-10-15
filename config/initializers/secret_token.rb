ad72e551b1c5fb49f2269decf79dea2f67705195
wyu <kharma@gmail.com>
2013-10-14 20:37:30 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Videoshare::Application.config.secret_token = 'ef4cb71f9d024d32128ba18892f64d05440fcfa750826223588e5dd0f315faf78a5b242056502207598039b3bf275cd3351efc1c465fdd0bc4f11ada9f4698af'
