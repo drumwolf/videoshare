b2c885019f05df7ec2aa6f62d525c22eb562c1c0
wyu <kharma@gmail.com>
2013-11-11 21:13:47 -0800

    59023950: stub controller in view spec

--- spec/views/video/index.html.haml_spec.rb
+++ spec/views/video/index.html.haml_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
-describe "video/index" do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "videos/index" do
+  before :each do
+    assign(:video, stub_model(Video,
+      :url => "http://youtu.be/Soo43Yu3G9o"
+    ).as_new_record)
+    assign(:videos, [
+      stub_model(Video,
+        :url => "http://youtu.be/Soo43Yu3G9o"
+      ),
+      stub_model(Video,
+        :url => "http://youtu.be/Soo43Yu3G9o"
+      )
+    ])
+  end
+
+  it "renders index view correctly" do
+    render
+  end
 end
