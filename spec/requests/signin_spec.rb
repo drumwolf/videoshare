f0b8d8136fcc5ba4b2598f824b4ca21c441bc16b
wyu <kharma@gmail.com>
2013-11-11 22:10:30 -0800

    59023950: add signin request spec

---
+++ spec/requests/signin_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+include ApplicationHelper
+include Warden::Test::Helpers
+Warden.test_mode!
+
+describe "Signin" do
+  describe "/users/sign_in" do
+    it 'renders correct landing page' do
+      page.should have_content 'Share Videos For Your Cause'
+    end
+  end
+
+  describe "GET /users/sign_in" do
+    before :each do
+      @user = FactoryGirl.create(
+        :user,
+        :email    => 'walter@stormsavvy.com',
+        :password => 'videogogo',
+        :password_confirmation => 'videogogo'
+      )
+      login_as(@user, :scope => :user)
+      visit new_user_session_path
+    end
+
+    it 'renders dashboard after login' do
+      page.should have_content 'Your Current YouTube Videos'
+      page.should have_content 'New YouTube Video'
+    end
+  end
+end
