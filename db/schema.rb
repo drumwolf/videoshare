1babd065c9a145e0cb3746d3bef3e4a4c2efebce
John Lee <drumwolf@gmail.com>
2013-12-18 21:35:11 -0800

    added migration: added user id to videos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131031030135) do
+ActiveRecord::Schema.define(:version => 20131219053358) do
 
   create_table "roles", :force => true do |t|
     t.string   "name"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(:version => 20131031030135) do
     t.string   "url"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
 end
